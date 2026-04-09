e420c2d803100ff925168b01de184b6131b03f75
juanrmv528e <juanramboy1@gmail.com>
2026-04-08 23:09:35 -0500

    20

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 def main():
     data = {}
-    
+
     print("Welcome to the Student Grades Manager!")
-    
+
     while True:
         print("\n1. Add student")
         print("2. Show all students")
         print("3. Show selected students")
         print("4. Exit")
-        
+
         option = input("Choose an option: ")
-        
+
         if option == "1":
             name = input("Enter student name: ")
-            data[name] = {}
-            print(f"{name} added.")
-        
+            proper = name.title()
+            data[proper] = {}
+            print(f"Student {proper} successfully added to the grades management system.")
+
         elif option == "2":
-            for student in data:
-                print(f"Student {name} successfully added to the grades management system.")
-        
+            if not data:
+                print("No students available.")
+            else:
+                for student in data:
+                    print(student)
+
         elif option == "3":
             names = input("Enter names separated by comma: ").split(",")
             names = [n.strip() for n in names]
-            
+
             for name in names:
-                if name in data:
-                    print(name)
+                proper = name.title()
+                if proper in data:
+                    print(proper)
                 else:
-                    print(f"{name} not found!")
-        
+                    print(f"{proper} not found!")
+
         elif option == "4":
             break
-        
+
         else:
             print("Invalid option selected!")
 
