89e79a3621ca23e452675dc424b1e885edec384d
juanrmv528e <juanramboy1@gmail.com>
2026-04-08 23:00:12 -0500

    13

--- grades_manager.py
+++ grades_manager.py
@@ -1,9 +1,34 @@
+def initialize_dict(name, grades):
+    return {name: grades}
+
+
+def add_student(data):
+    name = input("Enter student name: ")
+    data[name] = {}
+    print(f"Student {name} successfully added to the grades management system.")
+    return data
+
+
+def get_students(data, names):
+    result = {}
+    lower_map = {k.lower(): k for k in data}
+    
+    for name in names:
+        key = lower_map.get(name.lower())
+        if key:
+            result[key] = data[key]
+        else:
+            print(f"{name.capitalize()} not found!")
+    
+    return result
+
+
 def avg_by_student(data, selected=None):
-    lower_data = {k.lower(): k for k in data}
+    lower_map = {k.lower(): k for k in data}
     
     if selected:
         for name in selected:
-            key = lower_data.get(name.lower())
+            key = lower_map.get(name.lower())
             if key:
                 grades = data[key]
                 if grades:
